1cf39dcb8a9f68b07f7f876a57cdc7ad7ac2d528
yellhtetaung <yairhtet248.yha@gmail.com>
2022-12-28 13:55:05 +0630

    updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import { useState, useCallback } from "react";
-import { Lists } from "./data";
-import ToDo from "./ToDo";
+import { useState, useMemo } from "react";
+
+const calculation = (number) => {
+  console.log("Render Count");
+  for (let i = 0; i < 1000000; i++) {
+    number += 1;
+  }
+
+  return number;
+};
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const [todo, setTodo] = useState(Lists);
+  const [todo, setTodo] = useState([]);
+  const heavyCalculation = useMemo(() => calculation(count), [count]);
 
   const increase = () => {
     setCount((preCount) => preCount + 1);
   };
 
-  const deleteItem = useCallback(
-    (id) => {
-      setTodo(
-        todo.filter((t) => {
-          return t.id !== id;
-        })
-      );
-    },
-    [todo]
-  );
+  const addToDo = () => {
+    setTodo((todo) => {
+      return [...todo, { item: "New List added" }];
+    });
+  };
 
   return (
     <div>
-      <h1>useCallBack</h1>
+      <h1>useMemo</h1>
       <h4>Count:{count}</h4>
       <button onClick={increase}>Increase</button>
       <hr />
-      <ToDo todo={todo} deleteItem={deleteItem} />
+      <h2>Expensie/heavy Calculation</h2>
+      <button onClick={addToDo}>Add</button>
+      {todo.map((t, index) => {
+        return <p key={index}>{`${t.item} - ${index}`}</p>;
+      })}
+
+      <h1>{heavyCalculation}</h1>
     </div>
   );
 }
